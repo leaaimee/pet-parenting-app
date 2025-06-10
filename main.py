e138491dd47bdc806e01470ae8f31a5388101026
Lea <leaaimee2010@gmail.com>
2025-06-10 20:37:13 +0200

    add testign route

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from backend.routes.invitations_routes import router as invitations_router
 app = FastAPI()
 
 
+@app.get("/")
+async def root_check():
+    return {"message": "Pet Parenting API is live"}
+
+
+
 app.include_router(users_router, prefix="/api/v2/users", tags=["Users v2"])
 app.include_router(pets_router, prefix="/api/v2/pets", tags=["Pets"])
 app.include_router(invitations_router, prefix="/api/v2/invitations", tags=["Invitations"])
